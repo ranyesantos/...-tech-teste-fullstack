a5d3dbdc1448c242456cca58772b0781e907ae82
ranyesantos <lucasgts17@gmail.com>
2025-09-30 14:37:13 -0300

    feat(theme-mode): adding a theme switch button to allow switching between light and dark modes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('toggle-dark');
+    const html = document.documentElement;
+
+    if (localStorage.getItem('dark-mode') === 'true') {
+        html.classList.add('dark');
+    }
+
+    button.addEventListener('click', function () {
+        html.classList.toggle('dark');
+
+        if (html.classList.contains('dark')) {
+            localStorage.setItem('dark-mode', 'true');
+        } else {
+            localStorage.setItem('dark-mode', 'false');
+        }
+    });
+});
